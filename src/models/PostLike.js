4dfd2456aec111f34b50552703359fa816a7c2c2
Vaibhav Solanki <Vaibhavdasss@gmail.com>
2022-02-24 11:50:37 +0530

    Vaibhav 2nd

--- src/models/PostLike.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const postLikeSchema = new mongoose.Schema(
-  {
-    postId: { type: mongoose.Types.ObjectId, required: true, ref: "post" },
-    userId: [{ type: mongoose.Types.ObjectId, required: true, ref: "user" }],
-  },
-  { versionKey: false, timestamps: false }
-);
-module.exports = mongoose.model("postLike", postLikeSchema);
