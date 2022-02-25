177ad075acee77734c7a345bf43fc714f6611526
Vaibhav Solanki <Vaibhavdasss@gmail.com>
2022-02-25 16:54:00 +0530

    Update db.js

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,7 +1,6 @@
-require("dotenv").config();
 const mongoose = require("mongoose");
 module.exports = () => {
   return mongoose.connect(
-    `mongodb+srv://VaibhavDasss:@V2480d@onenetwork.ozbbj.mongodb.net/croma?authSource=admin&replicaSet=atlas-41l93k-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true`
+    "mongodb+srv://VaibhavDasss:V2480d@onenetwork.ozbbj.mongodb.net/S3D1?authSource=admin&replicaSet=atlas-41l93k-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true"
   );
 };
